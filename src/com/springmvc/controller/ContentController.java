82e2dae5b29d1920e0ae42b8cab3824ab6b0b2bc
liuhongfeng <liuhongfeng@liuhf>
2015-12-15 12:53:29 +0800

    添加文章控制类的注释

--- src/com/springmvc/controller/ContentController.java
+++ src/com/springmvc/controller/ContentController.java
@@ -1,13 +1,14 @@
 package com.springmvc.controller;
 
-import com.alibaba.fastjson.JSON;
-import com.alibaba.fastjson.JSONArray;
-import com.alibaba.fastjson.JSONObject;
-import com.alibaba.fastjson.serializer.URLCodec;
-import com.apicloud.sdk.api.Resource;
-import com.springmvc.dao.ArticleDao;
-import com.springmvc.dao.ColumnDao;
-import com.springmvc.data.Init;
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+import java.net.URLDecoder;
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.ui.ModelMap;
@@ -17,25 +18,41 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONObject;
+import com.apicloud.sdk.api.Resource;
+import com.springmvc.dao.ArticleDao;
+import com.springmvc.dao.ColumnDao;
+import com.springmvc.data.Init;
 
-import java.io.IOException;
-import java.io.UnsupportedEncodingException;
-import java.net.URLDecoder;
-import java.util.HashMap;
-import java.util.Map;
-
+/**
+* 类名: ContentController </br>
+* 包名： com.springmvc.controller
+* 描述: 文章类   </br>
+* 创建时间：  2015-12-15 </br>
+* 发布版本：V1.0  </br>
+ */
 @Controller
 @RequestMapping("content")
 public class ContentController {
+	
 	String autor;
+	
 	ArticleDao articleDao=new ArticleDao();
 	ColumnDao columnDao=new ColumnDao();
 
+	/**
+	* 方法名：list</br>
+	* 详述：文章列表 </br>
+	* 创建时间：2015-12-15  </br>
+	* @param model
+	* @param httpSession
+	* @return
+	* @throws UnsupportedEncodingException 说明返回值含义
+	* @throws 说明发生此异常的条件
+	 */
 	@RequestMapping(method = RequestMethod.GET)
 	public String  list(Model model,HttpSession httpSession) throws UnsupportedEncodingException {
-		
 		httpSession.getAttribute("admin");		
 		if(httpSession.getAttribute("admin")==null){
 			return "admin/login";
@@ -47,9 +64,17 @@ public class ContentController {
 		}	
 	}
 
+	/**
+	* 方法名：add</br>
+	* 详述：增加文章  </br>
+	* 创建时间：2015-12-15  </br>
+	* @param model
+	* @return
+	* @throws UnsupportedEncodingException 说明返回值含义
+	* @throws 说明发生此异常的条件
+	 */
 	@RequestMapping(value = "add",method = RequestMethod.GET)
 	public String add(ModelMap model) throws UnsupportedEncodingException {
-
 		model.addAttribute("list",columnDao.columnList());
 		return "content/add";
 	}
@@ -62,6 +87,21 @@ public class ContentController {
 		return "redirect:/content/";
 	}
 
+	/**
+	* 方法名：doUpdate</br>
+	* 详述：更新文章 </br>
+	* 创建时间：2015-12-15  </br>
+	* @param id
+	* @param title
+	* @param type
+	* @param column
+	* @param content
+	* @param img
+	* @param bot
+	* @return
+	* @throws UnsupportedEncodingException 说明返回值含义
+	* @throws 说明发生此异常的条件
+	 */
 	@RequestMapping(value = "do_update/{id}")
 	public String doUpdate(@PathVariable(value = "id")String id,@RequestParam(value = "title")String title,
 						   @RequestParam(value = "type")String type,@RequestParam(value = "column")String column,
@@ -71,6 +111,14 @@ public class ContentController {
 		return "redirect:/content/";
 	}
 
+	/**
+	* 方法名：delete</br>
+	* 详述：删除文章  </br>
+	* 创建时间：2015-12-15  </br>
+	* @param id
+	* @return 说明返回值含义
+	* @throws 说明发生此异常的条件
+	 */
 	@RequestMapping(value = "delete/{id}",method = RequestMethod.GET)
 	public String delete(@PathVariable(value = "id")String id){
 		articleDao.delete(id);
@@ -93,6 +141,7 @@ public class ContentController {
 		modelMap.addAttribute("test",json);
 		return "test/test";
 	}
+	
 	@RequestMapping(value = "checkObjectExists/{title}",method = RequestMethod.POST)
 	@ResponseBody
 	public Map<String, Object> checkObjectExists(HttpServletResponse response,@PathVariable(value = "title")String title) throws IOException {
